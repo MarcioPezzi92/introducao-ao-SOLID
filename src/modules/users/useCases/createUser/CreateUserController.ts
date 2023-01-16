4684e74c2ab658fe731c98b7505d88acf6c5dcd1
MarcioPezzi <marciopezzi92@gmail.com>
2023-01-16 00:03:37 -0300

    feat: create user controller

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,13 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const {name, email} = request.body;
+      const created = this.createUserUseCase.execute({name, email});
+      return response.status(201).json(created)
+    } catch (error) {
+      return response.status(400).json({error})
+    }
   }
 }
 
